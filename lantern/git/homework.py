c816e32d43ac6a37d6ce198a593f2410d5b8e2ac
Dima Serhiienko <dima.sergienko@gmail.com>
2020-03-13 18:26:18 +0200

    some upgrades

--- lantern/git/homework.py
+++ lantern/git/homework.py
@@ -48,7 +48,7 @@ def multiple_ints(first_value: int, second_value: int) -> int:
     Returns:
         Product of elements
     """
-    if isinstance(first_value, bool) or isinstance(second_value, bool):
+    if any(isinstance(i, bool) for i in [first_value, second_value]):
         raise TypeError
     elif all(isinstance(i, int) for i in [first_value, second_value]):
         return first_value * second_value

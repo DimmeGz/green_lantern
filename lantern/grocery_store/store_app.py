0ed0abddf65d603adfb984fac5a1c784042cca0a
Dima Serhiienko <dima.sergienko@gmail.com>
2020-04-10 00:03:52 +0300

    Homework done

--- lantern/grocery_store/store_app.py
+++ lantern/grocery_store/store_app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, request
-
 import inject
 
 
@@ -8,10 +7,20 @@ class NoSuchUserError(Exception):
         self.message = f'No such user_id {user_id}'
 
 
+class NoSuchStoreError(Exception):
+    def __init__(self, store_id):
+        self.message = f'No such store_id {store_id}'
+
+
 app = Flask(__name__)
 
 
 @app.errorhandler(NoSuchUserError)
+def my_error_handler(e):
+    return jsonify({'error': e.message}), 404
+
+
+@app.errorhandler(NoSuchStoreError)
 def my_error_handler(e):
     return jsonify({'error': e.message}), 404
 
@@ -30,8 +39,52 @@ def get_user(user_id):
     return jsonify(user)
 
 
-@app.route('/users/<int:user_id>', methods=['PATCH'])
+@app.route('/users/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
     db = inject.instance('DB')
     db.users.update_user_by_id(user_id, request.json)
     return jsonify({'status': 'success'})
+
+
+@app.route('/goods', methods=['POST'])
+def create_goods():
+    db = inject.instance('DB')
+    good_count = db.goods.add(request.json)
+    return jsonify({'numbers of items created': good_count}), 201
+
+
+@app.route('/goods')
+def get_goods():
+    db = inject.instance('DB')
+    goods = db.goods.get_goods()
+    return jsonify(goods)
+
+
+@app.route('/goods', methods=['PUT'])
+def update_goods():
+    db = inject.instance('DB')
+    upd, err = db.goods.update_goods(request.json)
+    return jsonify({'successfully_updated': upd, 'errors': {'no such id in goods': err}})
+
+
+@app.route('/stores', methods=['POST'])
+def post_store():
+    db = inject.instance('DB')
+    db.users.get_user_by_id(request.json['manager_id'])
+    store_id = db.stores.add(request.json)
+    return jsonify({'store_id': store_id}), 200
+
+
+@app.route('/stores/<int:store_id>')
+def get_store_by_id(store_id):
+    db = inject.instance('DB')
+    store = db.stores.get(store_id)
+    return jsonify(store)
+
+
+@app.route('/stores/<int:store_id>', methods=['PUT'])
+def update_store_by_id(store_id):
+    db = inject.instance('DB')
+    db.users.get_user_by_id(request.json['manager_id'])
+    db.stores.update(store_id, request.json)
+    return {'status': 'success'}, 201
--- lantern/grocery_store/test_store.py
+++ lantern/grocery_store/test_store.py
@@ -1,4 +1,6 @@
 import inject
+import json
+import pytest
 
 from store_app import app
 from fake_storage import FakeStorage
@@ -12,7 +14,6 @@ def configure_test(binder):
 class Initializer:
     def setup(self):
         inject.clear_and_configure(configure_test)
-
         app.config['TESTING'] = True
         with app.test_client() as client:
             self.client = client
@@ -53,7 +54,7 @@ class TestUsers(Initializer):
             json={'name': 'John Doe'}
         )
         user_id = resp.json['user_id']
-        resp = self.client.patch(
+        resp = self.client.put(
             f'/users/{user_id}',
             json={'name': 'Johanna Doe'}
         )
@@ -61,8 +62,99 @@ class TestUsers(Initializer):
         assert resp.json == {'status': 'success'}
 
     def test_unexistant_put_user(self):
-        resp = self.client.patch(
+        resp = self.client.put(
             f'/users/56',
             json={'name': 'John Doe'}
         )
         assert resp.status_code == 404
+
+
+class TestGoods(Initializer):
+    def test_post_goods(self):
+        with open('jsons/goods.json') as goods:
+            resp = self.client.post(
+                '/goods',
+                json=json.load(goods)
+            )
+        assert resp.status_code == 201
+        assert resp.json == {'numbers of items created': 10}
+
+    def test_get_goods(self):
+        with open('jsons/goods.json') as goods:
+            self.client.post(
+                '/goods',
+                json=json.load(goods)
+            )
+        resp = self.client.get('/goods')
+        assert resp.status_code == 200
+        with open('jsons/goods_updated.json') as new_goods:
+            assert resp.json == json.load(new_goods)
+
+    def test_update_goods(self):
+        with open('jsons/goods.json') as goods:
+            self.client.post(
+                '/goods',
+                json=json.load(goods)
+            )
+        with open('jsons/goods_for_update.json') as update:
+            resp = self.client.put(
+                '/goods',
+                json=json.load(update)
+            )
+            assert resp.json == {'successfully_updated': 3,
+                                 'errors': {'no such id in goods': [11, 12, 13]}}
+        resp = self.client.get('/goods')
+        assert resp.status_code == 200
+        with open('jsons/goods_updated_2.json') as new_goods:
+            assert resp.json == json.load(new_goods)
+
+
+class TestStores(Initializer):
+    @pytest.mark.parametrize(
+        "store,answer_code,answer",
+        (
+                ({'name': 'Mad Cow', 'location': 'Lviv', 'manager_id': 1}, 200, {'store_id': 1}),
+                ({'name': 'Mad Cow', 'location': 'Lviv', 'manager_id': 15}, 404, {'error': 'No such user_id 15'})
+        )
+    )
+    def test_post_store(self, store, answer_code, answer):
+        user = {'name': 'John Doe'}
+        self.client.post('/users', json=user)
+        resp = self.client.post('/stores', json=store)
+        assert resp.status_code == answer_code
+        assert resp.json == answer
+
+    @pytest.mark.parametrize(
+        "adress,answer_code,answer",
+        (
+                (1, 200, {'name': 'Mad Cow', 'location': 'Lviv', 'manager_id': 1}),
+                (2, 404, {'error': 'No such store_id 2'})
+        )
+    )
+    def test_get_store_by_id(self, adress, answer_code, answer):
+        user = {'name': 'John Doe'}
+        self.client.post('/users', json=user)
+        newstore = {'name': 'Mad Cow', 'location': 'Lviv', 'manager_id': 1}
+        self.client.post('/stores', json=newstore)
+        resp = self.client.get(f'/stores/{adress}')
+        assert resp.status_code == answer_code
+        assert resp.json == answer
+
+    @pytest.mark.parametrize(
+        "update,adress,answer_code,answer",
+        (
+                ({'name': 'Local Taste', 'location': 'Lviv', 'manager_id': 1}, 1, 201, {'status': 'success'}),
+                ({'name': 'Local Taste', 'location': 'Lviv', 'manager_id': 1}, 21, 404,
+                 {'error': 'No such store_id 21'}),
+                ({'name': 'Local Taste', 'location': 'Lviv', 'manager_id': 15}, 1, 404, {'error': 'No such user_id 15'})
+
+        )
+    )
+    def test_update_store_by_id(self, update, adress, answer_code, answer):
+        user = {'name': 'John Doe'}
+        self.client.post('/users', json=user)
+        newstore = {'name': 'Mad Cow', 'location': 'Lviv', 'manager_id': 1}
+        self.client.post('/stores', json=newstore)
+        resp = self.client.put(f'/stores/{adress}', json=update)
+        assert resp.status_code == answer_code
+        assert resp.json == answer

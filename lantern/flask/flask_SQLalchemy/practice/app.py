1803c00d3f3c721235b8f17a3606f34a0b114e52
DimmeX <dima.sergienko@gmail.com>
2020-05-08 13:27:49 +0300

    Delete print, add logging

--- lantern/flask/flask_SQLalchemy/practice/app.py
+++ lantern/flask/flask_SQLalchemy/practice/app.py
@@ -1,42 +1,42 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_utils import create_database, drop_database, database_exists
-
 from config import Config
-from populate_data import get_users, get_stores, get_goods
-from models import User, Store, Good, db
+from flask import Flask
+import logging
 
+from models import User, Store, Good, db
+from populate_data import get_users, get_stores, get_goods
+from sqlalchemy_utils import create_database, database_exists
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
+logging.basicConfig(filename="logfile.log", level=logging.INFO)
 
 with app.app_context():
     if database_exists(db.engine.url):
         db.create_all()
-        print('Database exists')
+        logging.info('Database exists')
     else:
-        print(f"Database does not exists {db.engine.url}")
+        logging.info('Database does not exists')
         create_database(db.engine.url)
-        print('Data base created')
+        logging.info('Data base created')
 
 with app.app_context():
     users = get_users()
     for user in users:
         db.session.add(User(**user))
     db.session.commit()
-    print('Data written in data_base succesfuly')
+    logging.info('Data written in data_base succesfuly')
 
 with app.app_context():
     stores = get_stores()
     for store in stores:
         db.session.add(Store(**store))
     db.session.commit()
-    print('Data written in data_base succesfuly')
+    logging.info('Data written in data_base succesfuly')
 
 with app.app_context():
     goods = get_goods()
     for good in goods:
         db.session.add(Good(**good))
     db.session.commit()
-    print('Data written in data_base succesfuly')
+    logging.info('Data written in data_base succesfuly')

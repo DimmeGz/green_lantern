85bc039bb1685f295adb3ba9d1cdb3158811cc39
Dima Serhiienko <dima.sergienko@gmail.com>
2020-06-09 23:10:24 +0300

    Add create and delete article forms, add article list view, add article and article list json view

--- lantern/django/django_intro/src/apps/articles/forms.py
+++ lantern/django/django_intro/src/apps/articles/forms.py
@@ -7,3 +7,8 @@ class SearchForm(forms.Form):
 
 class ArticleImageForm(forms.Form):
     image = forms.ImageField(required=False)
+
+
+class ArticleForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    body = forms.CharField(widget=forms.Textarea, label='Text')
--- lantern/django/django_intro/src/apps/articles/views.py
+++ lantern/django/django_intro/src/apps/articles/views.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.urls import reverse
 from django.views import View
-from django.views.generic import TemplateView, DetailView, FormView
+from django.views.generic import TemplateView, DetailView, FormView, ListView
 
-from apps.articles.forms import ArticleImageForm
+from apps.articles.forms import ArticleImageForm, ArticleForm
 from apps.articles.models import Article
+from django.core import serializers
 
 
 def main_page(request, some_id=None, *args, **kwargs):
@@ -77,4 +78,53 @@ class ArticleUpdateImageView(FormView):
 
         ctx['id'] = self.kwargs['id']
 
+        return ctx
+
+
+def delete_view(request, id):
+    obj = get_object_or_404(Article, id=id)
+    if request.method == "POST":
+        obj.delete()
+        return HttpResponseRedirect("/articles/results/")
+
+    return render(request, "delete_view.html")
+
+
+def article_json(request, id):
+    data = serializers.serialize("json", [Article.objects.get(pk=id)])
+    return HttpResponse(data)
+
+
+def articles_list_json(request):
+    return JsonResponse(list(Article.objects.all().values()), safe=False)
+
+
+class NewArticleFormView(FormView):
+    template_name = 'article_form.html'
+    form_class = ArticleForm
+    success_url = '/articles/post/'
+
+
+def new_article(request):
+    form = ArticleForm(data=request.POST)
+
+    if form.is_valid():
+        valid_article = form.cleaned_data
+        new_article = Article(**valid_article)
+        new_article.save()
+
+    return HttpResponseRedirect("/articles/results/")
+
+
+class ValidationError(object):
+    pass
+
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
         return ctx
